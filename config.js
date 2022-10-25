2107ea04d43e2741977b40806f38dbd445290687
santonocoffee <116608845+santonocoffee@users.noreply.github.com>
2022-10-25 10:10:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycby96dDgXyYFTPiRXttKeuWfIQNovFzP12ki8EHhcgd0Xgcf_JvpocbU9ufRN-X76QGE/exec";
